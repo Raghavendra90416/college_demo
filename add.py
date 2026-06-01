c499431f57729780d646d3ba3bbe8f7e82b21230
Srikanth parikibanda <parikibandasrikanthcse@gmail.com>
2026-06-01 20:36:04 +0530

    Modify add function to perform addition with user input

--- add.py
+++ add.py
@@ -1,2 +1,6 @@
 def add():
-    print("added")
+    a = int(input("Enter your first number : "))
+    b = int(input("Enter your secound number : "))
+    c = a + b
+    print(f"Adition of {a} & {b} is : {c}")
+
